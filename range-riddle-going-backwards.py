5ee48d925db70dcf01142ef01ef0cf9a3ff70e1d
balkanarchist <balkanarchist@yandex.com>
2024-03-08 09:01:58 -0700

    added code to range-riddle-going-backwards

---
+++ range-riddle-going-backwards.py
@@ -0,0 +1,9 @@
+'''
+Create a countdown timer that starts from 10 and counts down to 1.
+Use the range() function to generate the countdown sequence. Each
+number should be printed on a new line. This task will help you
+understand how to generate a decreasing sequence with range().
+'''
+
+for i in range(10, 0, -1):
+    print(i)
