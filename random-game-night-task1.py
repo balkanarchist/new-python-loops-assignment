61777567d34a7e0423786c3f7dfd5d5186958b7e
balkanarchist <balkanarchist@yandex.com>
2024-03-08 23:18:47 -0700

    added comment to game night task1

---
+++ random-game-night-task1.py
@@ -0,0 +1,40 @@
+'''
+Using the provided code snippet, simulate rolling a six-sided die 10 times.
+Extend the simulation to keep track of how many times each number is rolled.
+After the simulation, print out the frequency of each number.
+
+import random
+
+# Initialize a dictionary to keep track of dice roll frequencies
+roll_count = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+
+# Let's roll the dice 10 times
+for _ in range(10):
+    dice_roll = random.randint(1, 6)
+    # Update the roll_count dictionary
+    roll_count[dice_roll] += 1
+    print("You rolled a " + str(dice_roll) + "!")
+
+# Print out the frequency of each number
+for number, frequency in roll_count.items():
+    print(f"Number {number} was rolled {frequency} times.")
+'''
+
+import random
+
+# Initialize a dictionary to keep track of dice roll frequencies
+roll_count = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+
+# Let's roll the dice 10 times
+for _ in range(10):
+    dice_roll = random.randint(1, 6)
+    # Update the roll_count dictionary
+    roll_count[dice_roll] += 1
+    print("You rolled a " + str(dice_roll) + "!")
+
+# Print out the frequency of each number
+for number, frequency in roll_count.items():
+    print(f"Number {number} was rolled {frequency} times.")
+
+# This is the same code that was provided on the assignment page - nothing was added but it runs
+# as expected. If I'm meant to change the code somehow, that wasn't clear in the directions.
