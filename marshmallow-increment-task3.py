779c266b96453e894057928943e140b1c2491dfe
balkanarchist <balkanarchist@yandex.com>
2024-03-08 21:37:44 -0700

    revised code and comments for marshmallow task3

--- marshmallow-increment-task3.py
+++ marshmallow-increment-task3.py
@@ -7,6 +7,20 @@ importance of increment placement in preventing such errors.
 '''
 
 marshmallows_in_bag = 0
-while marshmallows < 5:
-    marshmallows += 1
-    print("Added a marshmallow! Now there are " + str(marshmallows) + " marshmallows.")
+while marshmallows_in_bag < 10:
+    print("I count " + str(marshmallows_in_bag) + " marshmallows in the bag.")
+    marshmallows_in_bag += 1
+
+# The code results in an error where there are too few marshmallows in the bag, due to the
+# increment being placed after the print statement. It begins with 0 because the first
+# marshmallow was added after the print statement. It also stopped too early (at 9) because
+# of the increment placement. Moving that before the print statement will fix the code.
+
+print() #added for separation and readability between the two versions of the code
+
+marshmallows_in_bag = 0
+while marshmallows_in_bag < 10:
+    marshmallows_in_bag += 1
+    print("I count " + str(marshmallows_in_bag) + " marshmallows in the bag.")
+
+# The code above ends when 1 marshmallows as there should be. The code has been corrected.
